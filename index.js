c6f81da1485602a25583cb05b162db03511c1065
mullaiventhan <mullaiventhan.er@gmail.com>
2024-05-31 19:40:22 +0530

    db connection established and routers

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-import express from "express"
+//buildin imports
+import express from "express";
+import dontenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
 
+//middleware 
 const app =express();
-const PORT=4000;
+dontenv.config();
+const PORT= process.env.PORT ||3000;
+const CONNECTION_STRING =process.env.CONNECTION_STRING;
+app.use(express.json({limit:"30mb",extended:"true"}));
+app.use(express.urlencoded({limit:"30mb",extended:"true"}));
+app.use(cors())
 
-app.listen(PORT,()=>{
-    console.log(`the port is running on ${PORT}`)
-});
+// app.listen(PORT,()=>{
+//     console.log("port is running default");
+// })
+//test api
+app.get("/test",(req,res)=>{
+    res.json({"msg":"testing"});
+    console.log(`test api`)
+}) 
 
-app.get("/user",(req,res)=>{
-    console.log("api working");
-    res.json({"msg":"api is working"})
-})
+//port listen
+
+mongoose.connect(CONNECTION_STRING)
+.then(()=>app.listen(PORT,()=>{console.log(`port is running on ${PORT}`);}))
+.catch(err=>console.log(err))
 
