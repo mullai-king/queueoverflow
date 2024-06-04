8cfdb2594ccb77d56716b8f9a9357fc8f3b7108c
mullaiventhan <mullaiventhan.er@gmail.com>
2024-06-04 17:29:12 +0530

    cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT= process.env.PORT ||3000;
 const CONNECTION_STRING =process.env.CONNECTION_STRING;
 app.use(express.json({limit:"30mb",extended:"true"}));
 app.use(express.urlencoded({limit:"30mb",extended:"true"}));
-app.use(cors())
+app.use(cors({origin:"*"}));
 
 //routes 
 app.use("/user",userRouter);
